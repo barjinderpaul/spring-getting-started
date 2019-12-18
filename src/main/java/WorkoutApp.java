099acb10406f5ab46673426a92f6894771f21a46
barjinderpaul <barjinderpaul.singh@gmail.com>
2019-12-18 14:42:29 +0530

    Made beans self enclosing and added bean for trackCoach

--- src/main/java/WorkoutApp.java
+++ src/main/java/WorkoutApp.java
@@ -6,8 +6,9 @@ public class WorkoutApp {
 
         Coach cricketCoach = (Coach) context.getBean("cricketCoach");
         // or no need to cast
-        Coach cricketCoach2 = context.getBean("cricketCoach", Coach.class);
+        Coach trackCoach = context.getBean("trackCoach", Coach.class);
 
-        System.out.println(cricketCoach2.getWorkout());
+        System.out.println(cricketCoach.getWorkout());
+        System.out.println(trackCoach.getWorkout());
     }
 }
